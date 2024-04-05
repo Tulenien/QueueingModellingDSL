e45e4d7524090f3950788f50dd1402c2208e9ed7
Tulenien <timofey.evsigneev@gmail.com>
2024-04-05 09:48:50 +0200

    feat: add instantiating logic

---
+++ qmodel.py
@@ -0,0 +1,202 @@
+from os.path import dirname, join
+import random
+from abc import ABC, abstractmethod
+
+from textx import metamodel_from_file
+from textx.export import metamodel_export, model_export
+
+class Request:
+    def __init__(self, name, generation_time, status):
+        self.name = name
+        self.generation_time = generation_time
+        self.start_processing_time = 0.0
+        self.finish_processing_time = 0.0
+        self.status = status
+        
+    def set_start_processing_time(self, time):
+        self.start_processing_time = time
+        
+    def set_finish_processing_time(self, time):
+        self.finish_processing_time = time
+        
+    def set_status(self, status):
+        self.status = status
+
+class RandomGenerator(ABC):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    @abstractmethod
+    def generate_double(self):
+        pass
+        
+class UniformDistribution(RandomGenerator):
+    def generate_double(self):
+        random_value = random.random()
+        result = self.a + (self.b - self.a) * random_value
+        return result
+        
+class NormalDistribution(RandomGenerator):
+    def generate_double():
+        result = 0.0
+        for i in range(12):
+            random_value = random.random()
+            result += random_value;
+        }
+        result -= 6
+        return abs(result * self.a * self.b);
+        
+class InformationSource:
+    def __init__(self, random_generator, number, name_template):
+        self.id = number
+        self.requests_generated = 0
+        self.current_time = 0.0
+        self.previous_time = 0.0
+        self.generator = random_generator
+        self.requests_name_template = name_template
+        
+    def generate_request(self):
+        requests_generated += 1
+        name = self.requests_name_template + str(self.requests_generated)
+        request = Request(name, current_time, "generated")
+        return request
+        
+    def get_current_time(self):
+        return self.current_time
+        
+    def get_previous_time(self):
+        return self.previous_time
+        
+    def get_requests_generated(self):
+        return requests_generated
+        
+    def generate_time(self):
+        self.previous_time = self.current_time
+        self.current_time += generator.generate_double()
+        
+    def get_id(self):
+        return id
+        
+class ProcessingUnit:
+    def __init__(self, generator, number, return_percent):
+        self.id = number
+        self.active = False
+        self.return_percent = return_percent
+        self.current_time = 0
+        self.previous_time = 0
+        self.return_count = 0
+        self.finished_count = 0
+        self.generator = generator
+        self.connected_inf_sources = []
+        
+    def process_request(self, request):
+        self.active = True
+        request.set_start_processing_time(self.current_time)
+        self.current_time += generator.generate_double()
+        request.set_status("processed")
+        request.set_finish_processing_time(current_time)
+        finished_count += 1
+        if self.return_percent > 0:
+            percent = random.randrange(100)
+            if percent < self.return_percent:
+                return_count += 1
+                return True
+        return False
+        
+    def is_active(self):
+        return self.active
+        
+    def get_current_time(self):
+        return self.current_time
+        
+    def get_finished_count(self):
+        return self.finished_count
+        
+    def get_return_count(self):
+        return self.return_count
+        
+    def set_current_time(self, time):
+        self.current_time = time
+        
+    def set_active(self, active):
+        self.active = active
+        
+    def connect_inf_source(self, source):
+        self.connected_inf_sources.append(source)
+    
+    def get_id(self):
+        return id
+
+class QSystem:
+    INF_SOURCE = "INF_SOURCE"
+    PROCESSOR = "PROCESSOR"
+    DELTA = 1e-5
+    def __init__(self):
+        self.system_modules = dict()
+        self.system_modules[INF_SOURCE] = []
+        self.system_modules[PROCESSOR] = []
+        self.time_constraint = 0.0
+        self.requests_constraint = 0
+        self.isTimed = False
+        self.global_time = 0.0
+        self.inf_source_seq = 0
+        self.processors_seq = 0
+        
+    def interpret(self, model):
+        # model is an instance of Program
+        for c in model.commands:
+            if c.__class__.__name__ == "SetTimeConstraint":
+                self.time_constraint = c.number
+                self.isTimed = True
+            elif c.__class__.__name__ == "SetRequestsConstraint":
+                self.requests_constraint = c.number
+            elif c.__class__.__name__ == "GenerateCommand":
+                generator = create_generator(c.distribution)
+                inf_source = InformationSource(generator, inf_source_seq, c.template)
+                self.system_modules[INF_SOURCE].append(inf_source)
+                inf_source_seq += 1
+            elif c.__class__.__name__ == "ProcessCommand":
+                generator = create_generator(c.distribution)
+                processor = ProcessingUnit(generator, processors_seq, c.percent, c.connected)
+                processors_seq += 1
+                flag = False
+                for source in c.connected:
+                    if flag:
+                        break
+                    for id in self.system_modules[INF_SOURCE]:
+                        if source.get_id() == id:
+                            processor.connect_inf_source(source)
+                            flag = True
+                            break
+                self.system_modules[PROCESSOR].append(processor)
+            
+    def create_generator(self, type):
+        if (type == "normal"):
+            generator = NormalDistribution()
+        elif(type == "uniform"):
+            generator = UniformDistribution()
+        return type
+
+
+def main(debug=False):
+
+    this_folder = dirname(__file__)
+
+    robot_mm = metamodel_from_file(join(this_folder, 'qsystem.tx'), debug=False)
+    metamodel_export(robot_mm, join(this_folder, 'qsystem_meta.dot'))
+
+# Example for adding commands
+# Use later for statistics generation
+    # Register object processor for MoveCommand
+    #robot_mm.register_obj_processors({'MoveCommand': move_command_processor})
+
+    qsystem_model = robot_mm.model_from_file(join(this_folder, 'program.qs'))
+    model_export(qsystem_model, join(this_folder, 'program.dot'))
+
+    system = QSystem()
+    system.interpret(qsystem_model)
+
+
+if __name__ == "__main__":
+    main()
